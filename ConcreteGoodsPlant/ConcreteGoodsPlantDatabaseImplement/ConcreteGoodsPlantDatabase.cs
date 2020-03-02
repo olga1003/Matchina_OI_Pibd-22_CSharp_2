adf26490cfdd88553fc2f03bc1646c2484a6c099
helga5622@gmail.com <helga5622@gmail.com>
2020-03-02 12:46:08 +0400

    3

---
+++ ConcreteGoodsPlant/ConcreteGoodsPlantDatabaseImplement/ConcreteGoodsPlantDatabase.cs
@@ -0,0 +1,25 @@
+﻿using ConcreteGoodsPlantDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcreteGoodsPlantDatabaseImplement
+{
+    public class AbstractShopDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=HOME\SQLEXPRESS;Initial
+Catalog=ConcreteGoodsPlantDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Product> Products { set; get; }
+        public virtual DbSet<ProductComponent> ProductComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
---
+++ ConcreteGoodsPlant/ConcreteGoodsPlantDatabaseImplement/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcreteGoodsPlantDatabaseImplement.Models
+{
+    public class Order
+    {
+    }
+}
