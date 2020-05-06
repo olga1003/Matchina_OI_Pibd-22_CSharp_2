1c8f336893f33bfe75a23f4f133b408bcc8482ba
helga5622@gmail.com <helga5622@gmail.com>
2020-05-06 12:34:10 +0400

    6

--- ConcreteGoodsPlant/ConcreteGoodsPlantDatabaseImplement/Models/Order.cs
+++ ConcreteGoodsPlant/ConcreteGoodsPlantDatabaseImplement/Models/Order.cs
@@ -9,21 +9,31 @@ namespace ConcreteGoodsPlantDatabaseImplement.Models
     public class Order
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
+
         public int ClientId { get; set; }
+
+        public int ProductId { get; set; }
+
         public int? ImplementerId { get; set; }
 
         [Required]
         public int Count { get; set; }
+
         [Required]
         public decimal Sum { get; set; }
+
         [Required]
         public OrderStatus Status { get; set; }
+
         [Required]
         public DateTime DateCreate { get; set; }
+
         public DateTime? DateImplement { get; set; }
-        public virtual Product Product { get; set; }
+
+        public Product Product { get; set; }
+
         public Client Client { get; set; }
+
         public Implementer Implementer { get; set; }
     }
 }
