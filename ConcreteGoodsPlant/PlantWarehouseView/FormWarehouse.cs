38a0e50ffe8dae6ead33519544c5a098393929ed
helga5622@gmail.com <helga5622@gmail.com>
2020-06-27 10:56:55 +0400

    5

--- ConcreteGoodsPlant/PlantWarehouseView/FormWarehouse.cs
+++ ConcreteGoodsPlant/PlantWarehouseView/FormWarehouse.cs
@@ -4,8 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
+
 using System.Windows.Forms;
 
 namespace PlantWarehouseView
